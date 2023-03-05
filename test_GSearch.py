116ec6eddfb98a5c267e80865dad31148891e521
Daria Kudriavtseva <situation96@mail.ru>
2023-03-05 19:41:45 +0300

    Making edits

--- test_GSearch.py
+++ test_GSearch.py
@@ -7,22 +7,24 @@ from selene import be, have
 def opening():
     browser.config.window_width = 1280
     browser.config.window_height = 720
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+
 
 
 @pytest.fixture()
 def opening_negative():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('dsfldkfdnlskdnglskglsngldjsnblksjnbldxn').press_enter()
+
 
 
 def test_search(opening):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_search_negative(opening_negative):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('dsfкариылпрnlskdnglskg%%%12787snblksjnbldxn').press_enter()
     browser.element('[id="result-stats"]').should(
         have.text('Результатов: примерно 0'))
